b1299e510279a82634de8e940157deeab9868934
Valery Shklovskiy <vs@MacBook-Pro-Valery.local>
2019-04-13 15:08:53 +0300

    v0.2 - added custome page, css, js

---
+++ page-vtest1.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package vstest1
+ */
+?>
+<!DOCTYPE html>
+<!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+<![endif]-->
+<head>
+  <meta charset="utf-8">
+	<title>VS Test</title>
+  <link rel="stylesheet" href="wp-content/themes/vstest1/style-vstest1.css">
+	<script type="text/javascript" src="wp-content/themes/vstest1/js/vstest1.js"></script>
+</head>
+
+<body>
+
+
+</body>
+</html>
