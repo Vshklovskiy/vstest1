4b68e897e5461f43b7524706c9c7ece506bd6a70
Valery Shklovskiy <vs@MacBook-Pro-Valery.local>
2019-04-13 15:26:48 +0300

    v0.5 - added big image overlay

--- page-vtest1.php
+++ page-vtest1.php
@@ -20,22 +20,29 @@
 <head>
   <meta charset="utf-8">
 	<title>VS Test</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="wp-content/themes/vstest1/style-vstest1.css?<? echo time(); ?>">
 	<script type="text/javascript" src="wp-content/themes/vstest1/js/vstest1.js?<? echo time(); ?>"></script>
 </head>
 
 <body>
 
-  <div id="parent" class="parent">
-    <div id="carousel" class="carousel">
-      <button class="arrow prev"><</button>
-      <div class="gallery">
-        <ul class="images" id="carousel_ul">
-        </ul>
-      </div>
-      <button class="arrow next">></button>
+<div id="parent" class="parent">
+  <div id="carousel" class="carousel">
+    <button class="arrow prev"><</button>
+    <div class="gallery">
+      <ul class="images" id="carousel_ul">
+      </ul>
     </div>
+    <button class="arrow next">></button>
   </div>
+</div>
+
+<div id="overlay">
+	<div id="overlayParent" class="parent">
+		<img id="overlayImg" src="https://via.placeholder.com/600/d32776">
+	</div>
+</div>
 
 </body>
 </html>
