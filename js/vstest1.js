3e59d95d1b2543bc52cfdc1163db14cf37ee71de
Valery Shklovskiy <vs@MacBook-Pro-Valery.local>
2019-04-13 15:24:05 +0300

    v0.4 - working prototype

---
+++ js/vstest1.js
@@ -0,0 +1,53 @@
+// get image links to images
+// > .albumId .id .title .url .thumbnailUrl
+    var xhr = new XMLHttpRequest()
+    xhr.open(
+      'GET',
+      'https://jsonplaceholder.typicode.com/photos',
+      true
+      )
+    xhr.send()
+
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState != 4) {
+        return
+      }
+
+      if (xhr.status === 200) {
+        console.log('images status: 200')
+        images = JSON.parse(xhr.responseText)
+      } else {
+        console.log('err', xhr.responseText)
+      }
+
+// insert images to carousel
+var list = document.getElementById('carousel_ul')
+for (var i = 0; i < images.length; i++) {
+  var li = document.createElement('li')
+  li.innerHTML = '<a class="link" data-url="'+images[i].url+'" href="#"><img src="'+images[i].thumbnailUrl+'"></a>'
+  list.appendChild(li)
+}
+
+document.getElementById('parent').style.display = 'flex';
+
+// conf
+var width = 150; // image width
+var count = 1; // image steps
+var position = 0; // start position
+
+var carousel = document.getElementById('carousel');
+var list = carousel.querySelector('ul');
+var listElems = carousel.querySelectorAll('li');
+
+// button actions
+carousel.querySelector('.prev').onclick = function() {
+  position = Math.min(position + width * count, 0)
+  list.style.marginLeft = position + 'px';
+};
+
+carousel.querySelector('.next').onclick = function() {
+  position = Math.max(position - width * count, -width * (listElems.length - count));
+  list.style.marginLeft = position + 'px';
+};
+
+    }
--- page-vtest1.php
+++ page-vtest1.php
@@ -20,12 +20,22 @@
 <head>
   <meta charset="utf-8">
 	<title>VS Test</title>
-  <link rel="stylesheet" href="wp-content/themes/vstest1/style-vstest1.css">
-	<script type="text/javascript" src="wp-content/themes/vstest1/js/vstest1.js"></script>
+  <link rel="stylesheet" href="wp-content/themes/vstest1/style-vstest1.css?<? echo time(); ?>">
+	<script type="text/javascript" src="wp-content/themes/vstest1/js/vstest1.js?<? echo time(); ?>"></script>
 </head>
 
 <body>
 
+  <div id="parent" class="parent">
+    <div id="carousel" class="carousel">
+      <button class="arrow prev"><</button>
+      <div class="gallery">
+        <ul class="images" id="carousel_ul">
+        </ul>
+      </div>
+      <button class="arrow next">></button>
+    </div>
+  </div>
 
 </body>
 </html>
